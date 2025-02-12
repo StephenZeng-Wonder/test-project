8a7ad4dae21091d5018425dff819dfac2c4d74e9
Stephen <stephenzeng@xm.wonder.com>
2025-02-12 16:19:27 +0800

    run test success

--- test1-module/src/test/java/org/example/TestUtil.java
+++ test1-module/src/test/java/org/example/TestUtil.java
@@ -2,13 +2,18 @@ package org.example;
 
 import org.example.util.StringUtil;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author stephen
  */
 class TestUtil {
+    private final Logger logger = LoggerFactory.getLogger(TestUtil.class);
+    
     @Test
     void test() {
         assert "aaa".equals(StringUtil.toLowerCase("AAA"));
+        logger.info("Test passed");
     }
 }
